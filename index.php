c4c2e8d95c9cc47748d9d15c9e03b1a453850de0
Luca Pellegrino <52167135+lucapellegrino70@users.noreply.github.com>
2022-02-21 15:45:21 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -41,117 +41,124 @@
 	$result = $db->query('SELECT * FROM tasks ORDER BY priority ASC, name ASC');
 	
 ?>
+<div class="container">
+	<h2>ToDo List</h2>
 
-<h2>ToDo List</h2>
-
-<?php if ($msg != "") { ?>
-	<div class="alert alert-success alert-dismissible fade show" role="alert">
-	  <?php echo $msg;?>
-	  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+	<?php if ($msg != "") { ?>
+		<div class="alert alert-success alert-dismissible fade show" role="alert">
+		  <?php echo $msg;?>
+		  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+		</div>
+	<?php } ?>
+	<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addTask">Add Task</button>
+	<div class="table-responsive">
+		<table class="table table-striped table-sm">
+		  <thead>
+			<tr>
+			  <th scope="col">#</th>
+			  <th scope="col">Name</th>
+			  <th scope="col">Priority</th>
+			  <th scope="col">Completed</th>
+			  <th scope="col">Actions</th>
+			</tr>
+		  </thead>
+		  <tbody>
+		  <?php 
+				$totalTasks=0;
+				$completedTasks = 0;
+		  
+				while ($row = $result->fetchArray()) {
+					$totalTasks++;
+					
+					if ((int)$row['priority'] == 0)
+						$clsBackground = "class='bg-danger text-white'";
+					else
+						$clsBackground = "class='bg-light'";
+					
+					if ($row['completed'] == 1)
+						$completedTasks++;
+					
+					echo "<tr $clsBackground>";
+					echo "<td>".$row['id']."</td>";
+					echo "<td>".$row['name']."</td>";
+					echo "<td>".getPriority($row['priority'])."</td>";
+					echo "<td>";
+					echo ($row['completed'] == 1) ? 'Yes' : 'No';
+					echo "</td>";
+					echo "<td><input type='button' class='btn btn-warning float-sm-start px-2' name='edit' id='edittask' value='Edit' data-id='".$row['id']."' data-task='".$row['name']."' data-priority='".$row['priority']."'>		
+					<form class='float-sm-start px-2' method='post' onsubmit='return confirmDelete();'><input type='hidden' name='id' value='".$row['id']."'><input type='hidden' name='action' value='delete'><input type='submit' class='btn btn-danger' name='submit' value='Delete'></form>";
+					echo ($row['completed'] == 0) ? "<form method='post'><input type='hidden' name='id' value='".$row['id']."'><input type='hidden' name='action' value='complete'><input type='submit' class='btn btn-success' name='complete' value='Complete'></form>" : "";
+					echo "</td>";
+					echo "</tr>";
+				}
+		 ?>		
+		  </tbody>
+		</table>
+		Total Tasks: <?php echo $totalTasks;?><br>
+		Completed Tasks: <?php echo $completedTasks;?><br>
+		
 	</div>
-<?php } ?>
-<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addTask">Add Task</button>
-<div class="table-responsive">
-	<table class="table table-striped table-sm">
-	  <thead>
-		<tr>
-		  <th scope="col">#</th>
-		  <th scope="col">Name</th>
-		  <th scope="col">Priority</th>
-		  <th scope="col">Completed</th>
-		  <th scope="col">Actions</th>
-		</tr>
-	  </thead>
-	  <tbody>
-	  <?php 
-			$totalTasks=0;
-			$completedTasks = 0;
-	  
-			while ($row = $result->fetchArray()) {
-				$totalTasks++;
-				if ($row['completed'] == 1)
-					$completedTasks++;
-				
-				echo "<tr>";
-				echo "<td>".$row['id']."</td>";
-				echo "<td>".$row['name']."</td>";
-				echo "<td>".getPriority($row['priority'])."</td>";
-				echo "<td>";
-				echo ($row['completed'] == 1) ? 'Yes' : 'No';
-				echo "</td>";
-				echo "<td><input type='button' class='btn btn-warning float-sm-start px-2' name='edit' id='edittask' value='Edit' data-id='".$row['id']."' data-task='".$row['name']."' data-priority='".$row['priority']."'>		
-				<form class='float-sm-start px-2' method='post' onsubmit='return confirmDelete();'><input type='hidden' name='id' value='".$row['id']."'><input type='hidden' name='action' value='delete'><input type='submit' class='btn btn-danger' name='submit' value='Delete'></form>";
-				echo ($row['completed'] == 0) ? "<form method='post'><input type='hidden' name='id' value='".$row['id']."'><input type='hidden' name='action' value='complete'><input type='submit' class='btn btn-success' name='complete' value='Complete'></form>" : "";
-				echo "</td>";
-				echo "</tr>";
-			}
-	 ?>		
-	  </tbody>
-	</table>
-	Total Tasks: <?php echo $totalTasks;?><br>
-	Completed Tasks: <?php echo $completedTasks;?><br>
-	
-</div>
 
-<div class="modal fade" id="addTask" tabindex="-1" aria-labelledby="addTaskLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title">Add Task</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-   	  <form method="post" id="form-add">
-      <div class="modal-body">
-			<label>Task Name</label>
-			<input type="text" class="form-control" name="task" placeholder="Insert the task name" required />
-			<label>Priority</label>
-			<select class="form-control" name="priority" required>
-				<option value="">Select Priority</option>
-				<option value="0">High</option>
-				<option value="1">Medium</option>
-				<option value="2">Low</option>
-			</select>
-			<input type="hidden" name="action" value="add" />
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="submit" class="btn btn-primary">Save</button>
-      </div>
-	  </form>
-    </div>
-  </div>
-</div>
+	<div class="modal fade" id="addTask" tabindex="-1" aria-labelledby="addTaskLabel" aria-hidden="true">
+	  <div class="modal-dialog">
+		<div class="modal-content">
+		  <div class="modal-header">
+			<h5 class="modal-title">Add Task</h5>
+			<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+		  </div>
+		  <form method="post" id="form-add">
+		  <div class="modal-body">
+				<label>Task Name</label>
+				<input type="text" class="form-control" name="task" placeholder="Insert the task name" required />
+				<label>Priority</label>
+				<select class="form-control" name="priority" required>
+					<option value="">Select Priority</option>
+					<option value="0">High</option>
+					<option value="1">Medium</option>
+					<option value="2">Low</option>
+				</select>
+				<input type="hidden" name="action" value="add" />
+		  </div>
+		  <div class="modal-footer">
+			<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+			<button type="submit" class="btn btn-primary">Save</button>
+		  </div>
+		  </form>
+		</div>
+	  </div>
+	</div>
 
 
-<div class="modal fade" id="editTask" tabindex="-1" aria-labelledby="editTaskLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title">Edit Task</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-   	  <form method="post" id="form-add">
-      <div class="modal-body">
-			<label>Task Name</label>
-			<input type="text" class="form-control" name="task" id="edit_task" placeholder="Insert the task name" required />
-			<label>Priority</label>
-			<select class="form-control" name="priority" id="edit_priority" required>
-				<option value="">Select Priority</option>
-				<option value="0">High</option>
-				<option value="1">Medium</option>
-				<option value="2">Low</option>
-			</select>
-			<input type="hidden" name="action" value="edit" />
-			<input type="hidden" name="id" id="edit_id" value="" />
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="submit" class="btn btn-primary">Save</button>
-      </div>
-	  </form>
-    </div>
-  </div>
-</div>
+	<div class="modal fade" id="editTask" tabindex="-1" aria-labelledby="editTaskLabel" aria-hidden="true">
+	  <div class="modal-dialog">
+		<div class="modal-content">
+		  <div class="modal-header">
+			<h5 class="modal-title">Edit Task</h5>
+			<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+		  </div>
+		  <form method="post" id="form-add">
+		  <div class="modal-body">
+				<label>Task Name</label>
+				<input type="text" class="form-control" name="task" id="edit_task" placeholder="Insert the task name" required />
+				<label>Priority</label>
+				<select class="form-control" name="priority" id="edit_priority" required>
+					<option value="">Select Priority</option>
+					<option value="0">High</option>
+					<option value="1">Medium</option>
+					<option value="2">Low</option>
+				</select>
+				<input type="hidden" name="action" value="edit" />
+				<input type="hidden" name="id" id="edit_id" value="" />
+		  </div>
+		  <div class="modal-footer">
+			<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+			<button type="submit" class="btn btn-primary">Save</button>
+		  </div>
+		  </form>
+		</div>
+	  </div>
+	</div>
+</div>	
 <?php 
 	function getPriority($priority) {
 			
