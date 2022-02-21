d65b6d9ff8f76fe417710cea4fc230d82596b409
Luca Pellegrino <52167135+lucapellegrino70@users.noreply.github.com>
2022-02-21 08:56:43 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,140 @@
+<?php 
+	require_once('layout/header.php');
+	require_once('config/config.php');
+
+	$db = sqlite_open('database/todo.db');
+	
+	$msg = "";
+	
+	if (isset($_POST["action"])) {
+		
+		if (isset($_POST['id'])) {
+			$id = $_POST["id"];
+			
+			if ($_POST["action"] == "delete") {
+				$sql = "DELETE FROM tasks WHERE id= ".$id;
+				$msg = "Task deleted!";
+			} else if ($_POST["action"] == "complete") {
+				$sql = "UPDATE tasks SET completed=1 WHERE id= ".$id;
+				$msg = "Task completed!";
+			}
+		}
+		
+		if ($_POST["action"] == "add") {
+			$priority = trim($_POST["priority"]);
+			$task = trim($_POST["task"]);
+			
+			$sql = "INSERT INTO tasks ('name', 'priority', 'completed') VALUES ('$task', $priority, 0)";
+			$msg = "Task created!";			
+		}	
+			
+		$result = $db->query($sql);		
+	}
+		
+	$result = $db->query('SELECT * FROM tasks ORDER BY priority ASC, name ASC');
+	
+?>
+
+<h2>ToDo List</h2>
+
+<?php if ($msg != "") { ?>
+	<div class="alert alert-success alert-dismissible fade show" role="alert">
+	  <?php echo $msg;?>
+	  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+	</div>
+<?php } ?>
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addTask">Add Task</button>
+<div class="table-responsive">
+	<table class="table table-striped table-sm">
+	  <thead>
+		<tr>
+		  <th scope="col">#</th>
+		  <th scope="col">Name</th>
+		  <th scope="col">Priority</th>
+		  <th scope="col">Completed</th>
+		  <th scope="col">Actions</th>
+		</tr>
+	  </thead>
+	  <tbody>
+	  <?php 
+			$totalTasks=0;
+			$completedTasks = 0;
+	  
+			while ($row = $result->fetchArray()) {
+				$totalTasks++;
+				if ($row['completed'] == 1)
+					$completedTasks++;
+				
+				echo "<tr>";
+				echo "<td>".$row['id']."</td>";
+				echo "<td>".$row['name']."</td>";
+				echo "<td>".getPriority($row['priority'])."</td>";
+				echo "<td>";
+				echo ($row['completed'] == 1) ? 'Yes' : 'No';
+				echo "</td>";
+				echo "<td><form class='float-sm-start px-2' method='post' onsubmit='return confirmDelete();'><input type='hidden' name='id' value='".$row['id']."'><input type='hidden' name='action' value='delete'><input type='submit' class='btn btn-danger' name='submit' value='Delete'></form>";
+				echo ($row['completed'] == 0) ? "<form method='post'><input type='hidden' name='id' value='".$row['id']."'><input type='hidden' name='action' value='complete'><input type='submit' class='btn btn-success' name='complete' value='Complete'></form>" : "";
+				echo "</td>";
+				echo "</tr>";
+			}
+	 ?>		
+	  </tbody>
+	</table>
+	Total Tasks: <?php echo $totalTasks;?><br>
+	Completed Tasks: <?php echo $completedTasks;?><br>
+	
+</div>
+
+<div class="modal fade" id="addTask" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">Add Task</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+   	  <form method="post" id="form-add">
+      <div class="modal-body">
+			<label>Task Name</label>
+			<input type="text" class="form-control" name="task" placeholder="Insert the task name" required />
+			<label>Priority</label>
+			<select class="form-control" name="priority" required>
+				<option value="">Select Priority</option>
+				<option value="0">High</option>
+				<option value="1">Medium</option>
+				<option value="2">Low</option>
+			</select>
+			<input type="hidden" name="action" value="add" />
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="submit" class="btn btn-primary">Save</button>
+      </div>
+	  </form>
+    </div>
+  </div>
+</div>
+
+<?php 
+	function getPriority($priority) {
+			
+		if ((int)$priority == 0)
+			return "High";
+		else if ((int)$priority == 1)
+			return "Medium";
+		else if ((int)$priority == 2)
+			return "Low";
+		
+	}	
+?>
+
+<script>
+	function confirmDelete() {
+		if (confirm('Do you really want to delete the record?'))
+			return true;
+		else
+			return false;
+	
+	}	
+
+</script>
+<?php require_once('layout/footer.php'); ?>
---
+++ layout/header.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Simple ToDo List in PHP & SQLITE">
+	<meta name="author" content="Luca Pellegrino">
+	
+	<title>ToDo List</title>
+
+	<!-- Bootstrap core CSS -->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css" crossorigin="anonymous">
+  </head>
+  <body>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
