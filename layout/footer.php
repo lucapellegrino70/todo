0afeafc88484c33b746dd0c3ed55f57d413f0350
Luca Pellegrino <52167135+lucapellegrino70@users.noreply.github.com>
2022-02-21 12:29:21 +0800

    Add files via upload

--- layout/footer.php
+++ layout/footer.php
@@ -1,7 +1,36 @@
 
 
 	</body>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 	<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+	
+	
+	<script>
+		$(document).ready(function() {
+
+			$('#edittask').click(function() {
+				var id = $(this).data("id");
+				var task = $(this).data("task");
+				var priority = $(this).data("priority");
+				
+				$('#edit_id').val(id);
+				$('#edit_task').val(task);
+				$('#edit_priority').val(priority);
+				
+				$('#editTask').modal('show');
+				
+			});
+		});
+
+		function confirmDelete() {
+			if (confirm('Do you really want to delete the record?'))
+				return true;
+			else
+				return false;
+		
+		}	
+
+	</script>
 </html>
 	
