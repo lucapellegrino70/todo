3fc28e9b0b6e4fbfec80ef83e40e1744e5e5e9e0
Luca Pellegrino <52167135+lucapellegrino70@users.noreply.github.com>
2022-02-21 16:36:20 +0800

    Add files via upload

--- config/config.php
+++ config/config.php
@@ -1,30 +1,12 @@
 <?php
+
+	// This function has a parameter $location that should contain the SQLite database path and name.
+	// Returns the handle of the database connection
 	function sqlite_open($location)
 	{
 		$handle = new SQLite3($location);
 		return $handle;
 	}
 	
-	function sqlite_query($dbhandle,$query)
-	{
-		$array['dbhandle'] = $dbhandle;
-		$array['query'] = $query;
-		$result = $dbhandle->query($query);
-		return $result;
-	}
-	
-	function sqlite_fetch_array(&$result,$type)
-	{
-		#Get Columns
-		$i = 0;
-		while ($result->columnName($i))
-		{
-			$columns[ ] = $result->columnName($i);
-			$i++;
-		}
-	   
-		$resx = $result->fetchArray(SQLITE3_ASSOC);
-		return $resx;
-	}
 		
 ?>
